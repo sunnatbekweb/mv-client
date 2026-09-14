a6715235f80330b319d0aed606e74b949b5654dc
sunnatbekweb <sunnatbekkadamov@gmail.com>
2026-09-14 23:23:30 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Header from './components/layout/Header.vue'
+import HomeWiew from './views/HomeWiew.vue'
 </script>
 
 <template>
 	<Header />
+	<HomeWiew />
 </template>
---
+++ src/views/HomeWiew.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section>
+		<div class="container">
+			<div class="">
+				<h3>The Perils of Pauline</h3>
+				<p>
+					Young Pauline is left a lot of money when her wealthy uncle dies.
+					However, her uncle's secretary has been named as her guardian until
+					she marries, at which time she will officially take ...
+				</p>
+			</div>
+		</div>
+	</section>
+</template>
